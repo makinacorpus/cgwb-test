e4b2dd5510272708ca2e08675c2d484fb26bbdb5
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-12-05 19:15:16 +0100

    up

--- clean.sh
+++ clean.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+cd $(dirname $0)
+rm -rf etc src
+if [[ -n $fulltest ]];then
+    tar xzvf $(ls -1rt ~/cgwb/testcgwb*z|head -n1)
+fi
+if [[ -z $no_clean ]];then
+    rm -rf .installed.cfg downloads/ .mr.developer.cfg parts/ bin/ develop-eggs/ eggs/*egg
+fi
 touch etc/sys/settings-local.cfg
-rm -rf .installed.cfg downloads/ .mr.developer.cfg parts/ bin/ develop-eggs/ eggs/*egg
 
