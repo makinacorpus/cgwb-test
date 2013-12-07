e5ca9170b2da2defc67d5c285dbd2d13fba724a7
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-12-07 19:19:58 +0100

    up

--- clean.sh
+++ clean.sh
@@ -2,12 +2,12 @@
 cd $(dirname $0)
 rm -rf etc src
 if [[ -n $fulltest ]];then
-    tar xzvf $(ls -1rt ~/cgwb/tata.toto.testcgwb*z|head -n1)
-    rm -rf src
-    cd tata.toto.testcgwb || exit -1
+    tar xzvf $(ls -1rt ~/cgwb/testcgwb*z|head -n1)
+    rm -rf src testcgwb
+    cd testcgwb || exit -1
     mv -f * .* ..
     cd ..
-    rm -rf tata.toto.testcgwb
+    rm -rf testcgwb
 fi
 if [[ -z $no_clean ]];then
     rm -rf .installed.cfg downloads/ .mr.developer.cfg parts/ bin/ develop-eggs/ eggs/*egg
